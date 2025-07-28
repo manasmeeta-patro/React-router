b0b55d057f4f34e4d201d07f676fa714abe42f3b
manasmeeta-patro <manasmitapatro20jan@gmail.com>
2025-07-28 15:56:28 +0530

    adding page not found for links

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes, Link } from "react-router"
 import Home from "./home";
 import About from "./about";
 import Login from "./login";
+import PageNotFound from "./pageNotFound";
 import "./app.css";
 
 function App() {
@@ -13,13 +14,17 @@ function App() {
    <Link className="link" to= "/"> Home </Link>
    <Link className="link" to="/login">Login</Link>
    <Link className="link" to="/about">About </Link>
+   
    </div>
    <br/><br/>
    <div className="routeClass">
   <Routes>
    <Route path="/about" element={<About/>}/>
    <Route path="/login" element={<Login/>}/>
-    <Route path="/" element={<Home/>}/>
+  <Route path="/" element={<Home/>}/>
+  {/* <Route path="/*" element={<Home/>}/>
+  <Route path="/*" element={<h2>page not found - 404</h2>}/> */}
+  <Route path="/*" element={<PageNotFound/>}/>
   </Routes>
   </div>
   </> 
